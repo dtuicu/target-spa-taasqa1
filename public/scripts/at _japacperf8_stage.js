51c3c10ea0adeef372789ecbe580cf9274b16b57
Ioana Postolache <postolac@adobe.com>
2022-03-30 13:52:50 +0300

    Added trigger views code for at.js

--- public/scripts/at _japacperf8_stage.js
+++ public/scripts/at _japacperf8_stage.js
@@ -6568,4 +6568,34 @@
     legacyBrowserSupport: !1,
   });
 
+function sanitizeViewName(viewName) {
+    if (viewName.startsWith("#")) {
+        viewName = viewName.substr(1);
+    }
+    if (viewName.startsWith("/")) {
+        viewName = viewName.substr(1);
+    }
+    return viewName;
+}
+function triggerView(viewName) {
+    viewName = sanitizeViewName(viewName) || "home";
+    // Validate if the Target Libraries are available on your website
+    if (
+        typeof adobe != "undefined" &&
+        adobe.target &&
+        typeof adobe.target.triggerView === "function"
+    ) {
+        adobe.target.triggerView(viewName);
+        console.log("AT: View triggered on page load: " + viewName);
+    }
+}
+//fire triggerView when the SPA loads and when the hash changes in the SPA
+if (window.location.pathname.indexOf("/") > -1) {
+    triggerView(location.hash);
+}
+window.onhashchange = function () {
+    if (window.location.pathname.indexOf("/") > -1) {
+        triggerView(location.hash);
+    }
+};
 //No Custom JavaScript
