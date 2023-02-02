c0f480e67b8a036c408e4674c45e8147fce3b0d8
dtuicu <61097727+dtuicu@users.noreply.github.com>
2023-02-02 15:12:14 +0200

    Delete redirect.js

--- src/pages/about/redirect.js
+++
@@ -1,7 +0,0 @@
-<script>
-    if(document.location.href.indexOf('param1=one&param2=two')>-1) {
-        //do nothing
-    } else  {
-        window.location.replace(document.location.search + (document.location.search.indexOf('?')>-1  ? '&param1=one&param2=two' : '?param1=one&param2=two'));
-    }
-</script>
